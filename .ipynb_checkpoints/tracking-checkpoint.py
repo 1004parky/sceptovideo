1234781ed8703f39cd9bc22dfe4248938fe00a0c
1004parky <1004parky@gmail.com>
2018-07-24 15:36:48 -0700

    Code now works for simple cases, now working on dealing with varying numbers of blobs

--- .ipynb_checkpoints/tracking-checkpoint.py
+++ .ipynb_checkpoints/tracking-checkpoint.py
@@ -1,85 +1,231 @@
 import numpy as np
 import pandas as pd
+import glob
 
 import skimage.io
+import scipy.spatial.distance
 
 import bootcamp_utils
 from behavioral_analysis import segmentation as seg
 
-#Takes an image and the bg image and returns the coordinates and sizes of the blobs of interest.
-def process_im(im, im_bg, thresh):
-    im_no_bg=seg.bg_subtract(im,im_bg)
-    im_bw, im_labeled, n_labels = seg.segment(im_no_bg)
-    #Filter blobs by size. Not too large, not too small.
+_kw_dict = {'c': 'centroid',
+            'e': 'eccentricity',
+            'j': 'major_axis_length',
+            'n': 'minor_axis_length',
+            'o': 'orientation',
+            'p': 'perimeter',
+            's': 'area'}
+
+def process_im(im, im_bg=None, size_thresh=[0, 0], props='soc'):
+    '''Function to identify properties of blobs of interest (BOI) from an image
+    
+    PARAMETERS
+    ----------
+    im: numpy.ndarray, a 2d array representing an image with BOI
+    size_thresh: list, a list with the lower and upper size bounds for a BOI.  
+    Default treats all blobs as BOI.  
+    im_bg: numpy.ndarray, a 2d array representing the image with no BOI
+    props: string of keyword characters for properties to extract about BOI.  
+    Default values are s(size), o(orientation), and c(centroid).  
+    Valid options:
+        | KEYWORD | MEANING           |
+        | c       | centroid          |
+        | e       | eccentricity      |
+        | j       | major_axis_length |
+        | n       | minor_axis_length |
+        | o       | orientation       |
+        | p       | perimeter         |
+        | s       | size              |
+    
+    RETURNS
+    -------
+    pandas.DataFrame of BOIs (rows) and desired attributes (cols)
+    '''
+    props = list(props)
+
+    # Error handling
+    if len(size_thresh) != 2:
+        raise Exception('size_thresh must have a lower and upper size limit')
+    if type(im) != np.ndarray:
+        raise Exception('im must be a 2d numpy ndarray')
+    for x in props:
+        if x not in _kw_dict:
+            raise Exception('Property keyword not recognized. See documentation.')
+
+    if size_thresh == [0, 0]:
+        thresholding = False
+    else:
+        thresholding = True
+
+    if im_bg:
+        im = seg.bg_subtract(im, im_bg)
+    im_bw, im_labeled, n_labels = seg.segment(im)
 
     #spec_labels is a list of all specimen [label, size, coordinates]
-    spec_labels=[]
-    properties=skimage.measure.regionprops(im_labeled)
-    
-    for i in range(0,n_labels):
-        if properties[i].area>thresh[0] and properties[i].area<thresh[1]: #about the size of ants/beetles
-            spec_labels.append([properties[i].area, properties[i].orientation, properties[i].centroid])
+    spec_labels = []
+    properties = skimage.measure.regionprops(im_labeled)
+
+    for i in range(0, n_labels):
+        if thresholding:
+            if properties[i]['area'] not in range(size_thresh[0], size_thresh[1]):
+                continue
+        spec_labels.append([ properties[i][_kw_dict[x]] ] for x in props)
             
-    return pd.DataFrame(data=spec_labels, columns=['size','orientation','centroid'])
+    return pd.DataFrame(data=spec_labels, 
+                        columns=[_kw_dict[x] for x in props])
 
-def match_blobs(oim, nim, db, size_weight=1, orient_weight=1, xy_weight=1):
-    #oim is old image, nim is new image. Match nim indices to oim indices.
-    #db is a dataframe of timepoints (rows) by specimen (column) filled with centroids.
-    assert oim.shape==nim.shape
-    assert type(db)==pd.core.frame.DataFrame
 
-    #number of specimen
-    n_spec=oim.shape[0]
+def match_blobs(oim, nim, center_coord, metrics='soc', 
+                weights=[1, 1, 1], same_weights=False, 
+                dist_func=scipy.spatial.distance.euclidean):
+    '''Function to match blobs across two images using user-specified metrics
 
-    #Matrix of comparison scores 
-    sizediff=[[np.nan for i in range(n_spec)] for j in range(n_spec)]
-    orientdiff=[[np.nan for i in range(n_spec)] for j in range(n_spec)]
-    xydiff=[[np.nan for i in range(n_spec)] for j in range(n_spec)]
+    PARAMETERS
+    ----------
+    oim: pandas.DataFrame with processed image data (see function process_im)
+    nim: pandas.DataFrame representing one timepoint after oim
+    center_coord: pandas.DataFrame, stores centroids for each blob (row) and timepoint (col)
+    metrics: str of metrics with which to match blobs.  
+    Defaults are s(size), o(orientation), and c(centroid distance). 
+        Valid options:
+        | KEYWORD | MEANING           |
+        | c       | centroid distance |
+        | e       | eccentricity      |
+        | j       | major_axis_length |
+        | n       | minor_axis_length |
+        | o       | orientation       |
+        | p       | perimeter         |
+        | s       | size              |
+    same_weights: boolean, True applies same weight (1) to all metrics
+    weights: list of weights corresponding respectively to given metrics
+    dist_func: string, a formula for calculating distance.  Default is euclidean.  
 
-    #calculate diff in size, orientation, and centroids for every combination of new blob and old blob
-    for n in range(n_spec):
-        for o in range(n_spec):
-            sizediff[n][o]=size_weight*(abs(oim.get('size')[o]-nim.get('size')[n]))/oim.get('size')[o]
-            orientdiff[n][o]=orient_weight*abs(oim.get('orientation')[o]-nim.get('orientation')[n])
-            dist_vec=np.subtract(oim.get('centroid')[o],nim.get('centroid')[n])
-            xydiff[n][o]=xy_weight*np.sqrt(np.dot(dist_vec,dist_vec)) #dot product of difference vector
+    RETURNS
+    -------
+    pandas.DataFrame with centroids for newest timepoint (nim) added
+    pandas.DataFrame, essentially nim with reordered rows to match oim's indexing
+    '''
+    metrics = list(metrics)
+    if same_weights:
+        weights = [1] * len(metrics)
 
-    #Add the differences up, want to minimize the sum of differences.
-    matchProb=np.add(np.add(sizediff,orientdiff),xydiff)
+    # Error handling
+    if not oim.shape[0] == nim.shape[0]:
+        raise Exception('oim and nim must have the same number of rows')
+    if not type(center_coord) == pd.core.frame.DataFrame:
+        raise Exception('center_coord must be a pandas.DataFrame')
+    if len(weights) != len(metrics):
+        raise Exception('Must provide a weight for each metric provided')
+    for x in metrics:
+        if x not in _kw_dict:
+            raise Exception('Metric keyword not recognized. See documentation.')
+    if not callable(dist_func):
+        raise Exception('Distance function is not callable.')
 
-    nlabels=np.argsort(np.argmin(matchProb, axis=1))    
+    # Number of specimen
+    n_spec = oim.shape[0]
+
+    # Initialize matrix of comparison scores
+    comp_matrix = [[[np.nan for i in range(n_spec)] 
+                    for j in range(n_spec)] 
+                    for k in range(len(metrics))]
+
+    # Fill in comparison matrix
+    for i in range(len(metrics)):
+        for n in range(n_spec):
+            for o in range(n_spec):
+                if metrics[i] == 'c':
+                    comp_matrix[n][o][i] = (weights[i] * 
+                        dist_func(oim.get(_kw_dict[metrics[i]])[o], 
+                                  nim.get(_kw_dict[metrics[i]])[n]))
+                else:
+                    comp_matrix[n][o][i] = (weights[i] * 
+                        abs(oim.get(_kw_dict[ metrics[i] ])[o] - 
+                        nim.get(_kw_dict[ metrics[i] ])[n]) /
+                        oim.get(_kw_dict[ metrics[i] ])[o])
+
+    # Add the differences up, want to minimize the sum of differences.
+    p_match = [[0 for i in range(n_spec)] for j in range(n_spec)]
+    for i in range((comp_matrix.shape)[2]):
+        p_match=np.add(p_match, comp_matrix[:][:][i])
+
+    n_labels = np.argsort(np.argmin(p_match, axis=1))    
     
-    #List of centroids in new order.
-    ncentroids=[nim.get('centroid')[k] for k in nlabels]
+    # List of centroids in new order.
+    n_centroids = [nim.get('centroid')[k] for k in n_labels]
 
-    new_tp=pd.Series(data=ncentroids)
-    return pd.concat([db,new_tp], axis=1, ignore_index=True), nim.reindex(nlabels)
+    new_tp = pd.Series(data=n_centroids)
+    return (pd.concat([center_coord, new_tp], axis=1, ignore_index=True), 
+                      nim.reindex(n_labels))
 
-"""
-def getDisp():
-    #Get displacement, just take first and last point.
 
-def getDist():
-    #Add up all distances traveled.
-    
-def getSpeed():
-    #Return a list of speeds
-    
-def getSociality():
-    #if any specimen within a certain radius of any other specimen, count as social behavior?
-    
-def trackImgs(fpath, bg_path, thresh)
-    all_files=glob.glob(fpath)
-    im_bg=skimage.io.imread(bg_path)
+def track(fpath, bg_path=None, size_thresh=[0, 0], metrics='soc', 
+          weights=[1, 1, 1], same_weights=False, 
+          dist_func=scipy.spatial.distance.euclidean):
+    '''Tracks the location of several blobs of interest across multiple images
 
-    data=pd.DataFrame()
-    processed_im=[]
+    PARAMETERS
+    ----------
+    fpath: str, absolute path of the directory containing images
+    bg_path: str, absolute path of the file
+    size_thresh: list, a list with the lower and upper size bounds for a BOI.  
+    Default treats all blobs as BOI. 
+    metrics: str of metrics with which to match blobs.  
+    Defaults are s(size), o(orientation), and c(centroid distance). 
+        Valid options:
+        | KEYWORD | MEANING           |
+        | c       | centroid distance |
+        | e       | eccentricity      |
+        | j       | major_axis_length |
+        | n       | minor_axis_length |
+        | o       | orientation       |
+        | p       | perimeter         |
+        | s       | size              |
+    same_weights: boolean, True applies same weight (1) to all metrics
+    weights: list of weights corresponding respectively to given metrics
+    dist_func: scipy.spatial.distance function.  Default is euclidean.  
+    Other options include 
 
-    for x in test_files:
-        im_test=skimage.io.imread(x)
-        processed_im.append(processImage(im_test,im_bg,thresh))
+    RETURNS
+    -------
+    data: pandas.DataFrame with centroids for every specimen (row) at each tp (col)
+    '''
+    if same_weights:
+        weights = [1] * len(metrics)
 
-    data=pd.concat([data, processed_im[0].get('centroid')], axis=1, ignore_index=True)
-    for j in range(len(processed_im)-1):
-        data, processed_im[j+1]=matchBlobs(processed_im[j], processed_im[j+1], data)
-"""
+    # Error handling
+    if len(weights) != len(metrics):
+        raise Exception('Must provide a weight for each metric provided')
+    try:
+        all_files = glob.glob(fpath)
+    except:
+        raise Exception('Directory of images not found.')
+    if bg_path:
+        try:
+            im_bg = skimage.io.imread(bg_path)
+        except:
+            raise Exception('Background image file not found')
+    else:
+        im_bg = None
+    if not callable(dist_func):
+        raise Exception('Distance function is not callable.')
+        
+
+    data = pd.DataFrame()
+    im_data = []
+
+    for x in all_files:
+        im_test = skimage.io.imread(x)
+        im_data.append(process_im(im_test, im_bg=im_bg, size_thresh=size_thresh))
+
+    data = pd.concat([data, im_data[0].get('centroid')], 
+                      axis=1, ignore_index=True)
+    for j in range(len(im_data)-1):
+        data, im_data[j+1] = match_blobs(im_data[j], 
+                                         im_data[j+1], 
+                                         data,
+                                         metrics=metrics, 
+                                         weights=weights,
+                                         same_weights=same_weights, 
+                                         dist_func=dist_func)
+    return data
--- tracking.py
+++ tracking.py
@@ -41,16 +41,19 @@ def process_im(im, im_bg=None, size_thresh=[0, 0], props='soc'):
     -------
     pandas.DataFrame of BOIs (rows) and desired attributes (cols)
     '''
-    props = props.split()
+    props = list(props)
 
     # Error handling
     if len(size_thresh) != 2:
         raise Exception('size_thresh must have a lower and upper size limit')
     if type(im) != np.ndarray:
-        raise Exception('im must be a 2d numpy ndarray')
+        raise Exception('im must be a 2d numpy ndarray.')
+    if len(props) != len(set(props)):
+        print('Warning: Some property keywords are repeated. Redundancies were removed.')
+        props = list(set(props))
     for x in props:
         if x not in _kw_dict:
-            raise Exception('Property keyword not recognized. See documentation.')
+            raise Exception('Property keyword "'+x+'" not recognized. See documentation.')
 
     if size_thresh == [0, 0]:
         thresholding = False
@@ -58,6 +61,8 @@ def process_im(im, im_bg=None, size_thresh=[0, 0], props='soc'):
         thresholding = True
 
     if im_bg:
+        if type(im_bg) != np.ndarray:
+            raise Exception('im_bg must be a 2d numpy ndarray.')
         im = seg.bg_subtract(im, im_bg)
     im_bw, im_labeled, n_labels = seg.segment(im)
 
@@ -67,16 +72,16 @@ def process_im(im, im_bg=None, size_thresh=[0, 0], props='soc'):
 
     for i in range(0, n_labels):
         if thresholding:
-            if properties[i]['area'] not in range(size_thresh):
+            if properties[i]['area'] not in range(size_thresh[0], size_thresh[1]):
                 continue
-        spec_labels.append([ properties[i][_kw_dict[x]] ] for x in props)
+        spec_labels.append([ properties[i][_kw_dict[x]]  for x in props])
             
     return pd.DataFrame(data=spec_labels, 
                         columns=[_kw_dict[x] for x in props])
 
 
-def match_blobs(oim, nim, center_coord, metrics='soc', 
-                weights=[1, 1, 1], same_weights=False, 
+def match_blobs(oim, nim, center_coord=pd.DataFrame(), metrics='soc', 
+                weights=None, same_weights=True, 
                 dist_func=scipy.spatial.distance.euclidean):
     '''Function to match blobs across two images using user-specified metrics
 
@@ -84,7 +89,8 @@ def match_blobs(oim, nim, center_coord, metrics='soc',
     ----------
     oim: pandas.DataFrame with processed image data (see function process_im)
     nim: pandas.DataFrame representing one timepoint after oim
-    center_coord: pandas.DataFrame, stores centroids for each blob (row) and timepoint (col)
+    center_coord: pandas.DataFrame, stores centroids for each blob (col) and timepoint (row).  
+    Default creates a blank DataFrame.
     metrics: str of metrics with which to match blobs.  
     Defaults are s(size), o(orientation), and c(centroid distance). 
         Valid options:
@@ -98,37 +104,47 @@ def match_blobs(oim, nim, center_coord, metrics='soc',
         | s       | size              |
     same_weights: boolean, True applies same weight (1) to all metrics
     weights: list of weights corresponding respectively to given metrics
-    dist_func: string, a formula for calculating distance.  Default is euclidean.  
+    dist_func: scipy.spatial.distance function.  Default is euclidean.  
+    Other options include .braycurtis, .canberra, .cityblock, etc.
 
     RETURNS
     -------
     pandas.DataFrame with centroids for newest timepoint (nim) added
     pandas.DataFrame, essentially nim with reordered rows to match oim's indexing
     '''
-    metrics = metrics.split()
-    if same_weights:
+    metrics = list(metrics)
+    if same_weights and (weights == None):
         weights = [1] * len(metrics)
 
     # Error handling
     if not oim.shape[0] == nim.shape[0]:
         raise Exception('oim and nim must have the same number of rows')
     if not type(center_coord) == pd.core.frame.DataFrame:
-        raise Exception('center_coord must be a pandas.DataFrame')
+        raise Exception('center_coord must be a pandas.DataFrame.')
     if len(weights) != len(metrics):
         raise Exception('Must provide a weight for each metric provided')
     for x in metrics:
         if x not in _kw_dict:
-            raise Exception('Metric keyword not recognized. See documentation.')
+            raise Exception('Metric keyword "'+x+'" not recognized. See documentation.')
+        try:
+            oim[(_kw_dict[x])]
+        except:
+            raise Exception('Old DataFrame does not contain data for metric '+_kw_dict[x])
+        try:
+            nim[(_kw_dict[x])]
+        except:
+            raise Exception('New DataFrame does not contain data for metric '+_kw_dict[x])
     if not callable(dist_func):
         raise Exception('Distance function is not callable.')
+    if len(metrics) != len(set(metrics)):
+        print('Warning: Some property keywords were repeated. Redundancies were removed')
+        metrics = list(set(metrics))
 
     # Number of specimen
     n_spec = oim.shape[0]
 
     # Initialize matrix of comparison scores
-    comp_matrix = [[[np.nan for i in range(n_spec)] 
-                    for j in range(n_spec)] 
-                    for k in range(len(metrics))]
+    comp_matrix = np.zeros((n_spec, n_spec, len(metrics)))
 
     # Fill in comparison matrix
     for i in range(len(metrics)):
@@ -136,31 +152,37 @@ def match_blobs(oim, nim, center_coord, metrics='soc',
             for o in range(n_spec):
                 if metrics[i] == 'c':
                     comp_matrix[n][o][i] = (weights[i] * 
-                        dist_func(oim.get(_kw_dict[metrics[i]])[o], 
-                                  nim.get(_kw_dict[metrics[i]])[n]))
+                        dist_func(oim[_kw_dict[metrics[i]]].values[o], 
+                                  nim[_kw_dict[metrics[i]]].values[n]))
                 else:
-                    comp_matrix[n][o][i] = (weights[i] * 
-                        abs(oim.get(_kw_dict[ metrics[i] ])[o] - 
-                        nim.get(_kw_dict[ metrics[i] ])[n]) /
-                        oim.get(_kw_dict[ metrics[i] ])[o])
+                    try:
+                        comp_matrix[n][o][i] = (weights[i] * 
+                            abs(oim[_kw_dict[metrics[i]]].values[o] - 
+                            nim[_kw_dict[metrics[i]]].values[n]) /
+                            oim[_kw_dict[metrics[i]]].values[o])
+                    except:
+                        comp_matrix[n][o][i] = (weights[i] * 
+                            abs(oim[_kw_dict[metrics[i]]].values[o] - 
+                            nim[_kw_dict[metrics[i]]].values[n]))
 
     # Add the differences up, want to minimize the sum of differences.
-    p_match = [[0 for i in range(n_spec)] for j in range(n_spec)]
-    for i in range((comp_matrix.shape)[2]):
-        p_match=np.add(p_match, comp_matrix[:][:][i])
+    p_match = np.array([[sum(comp_matrix[i][j][:]) 
+                        for j in range(n_spec)] for i in range(n_spec)])
 
-    n_labels = np.argsort(np.argmin(p_match, axis=1))    
+    new_labels = np.argsort(np.argmin(p_match, axis=1))    
     
     # List of centroids in new order.
-    n_centroids = [nim.get('centroid')[k] for k in n_labels]
+    n_centroids = [nim.get('centroid')[k] for k in new_labels]
+    new_tp = pd.DataFrame()
+    for i in range(n_spec):  
+        new_tp[i] = [n_centroids[i][:]]
 
-    new_tp = pd.Series(data=n_centroids)
-    return (pd.concat([center_coord, new_tp], axis=1, ignore_index=True), 
-                      nim.reindex(n_labels))
+    return (pd.concat([center_coord, new_tp], axis=0, ignore_index=True), 
+                      nim.reindex(new_labels))
 
 
 def track(fpath, bg_path=None, size_thresh=[0, 0], metrics='soc', 
-          weights=[1, 1, 1], same_weights=False, 
+          weights=None, same_weights=True, 
           dist_func=scipy.spatial.distance.euclidean):
     '''Tracks the location of several blobs of interest across multiple images
 
@@ -184,21 +206,20 @@ def track(fpath, bg_path=None, size_thresh=[0, 0], metrics='soc',
     same_weights: boolean, True applies same weight (1) to all metrics
     weights: list of weights corresponding respectively to given metrics
     dist_func: scipy.spatial.distance function.  Default is euclidean.  
-    Other options include 
+    Other options include .braycurtis, .canberra, .cityblock, etc.
 
     RETURNS
     -------
-    data: pandas.DataFrame with centroids for every specimen (row) at each tp (col)
+    data: pandas.DataFrame with centroids for every specimen (col) at each tp (row)
     '''
-    if same_weights:
+    if same_weights and (weights == None):
         weights = [1] * len(metrics)
+    all_files = glob.glob(fpath)
 
     # Error handling
     if len(weights) != len(metrics):
         raise Exception('Must provide a weight for each metric provided')
-    try:
-        all_files = glob.glob(fpath)
-    except:
+    if len(all_files) == 0:
         raise Exception('Directory of images not found.')
     if bg_path:
         try:
@@ -209,7 +230,6 @@ def track(fpath, bg_path=None, size_thresh=[0, 0], metrics='soc',
         im_bg = None
     if not callable(dist_func):
         raise Exception('Distance function is not callable.')
-        
 
     data = pd.DataFrame()
     im_data = []
@@ -217,9 +237,10 @@ def track(fpath, bg_path=None, size_thresh=[0, 0], metrics='soc',
     for x in all_files:
         im_test = skimage.io.imread(x)
         im_data.append(process_im(im_test, im_bg=im_bg, size_thresh=size_thresh))
+    
+    first_tp = pd.DataFrame(data = [im_data[0].get('centroid').tolist()])
+    data = pd.concat([data, first_tp], axis=0, ignore_index=True)
 
-    data = pd.concat([data, im_data[0].get('centroid')], 
-                      axis=1, ignore_index=True)
     for j in range(len(im_data)-1):
         data, im_data[j+1] = match_blobs(im_data[j], 
                                          im_data[j+1], 
